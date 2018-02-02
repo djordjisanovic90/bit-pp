3ab2f39711aabc4aeeadf9673ce2d1fe5f644dd1
BIT student <classroom@bgit.rs>
2018-02-02 12:43:43 +0100

    prvi commit

---
+++ najvecibroj.js
@@ -0,0 +1,21 @@
+var a = -6;
+var b = 37;
+var c = 7;
+
+
+if (a > b) {
+    console.log("Najveci broj je " + a);
+} else if (b > a && b > c) {
+    console.log("najveci broj je " + b);
+} else {
+    console.log("Najveci broj je " + c);
+}
+// drugi nacin
+var a = -6;
+var b = 37;
+var c = 7;
+var max;
+if( )
+
+
+//drugi primer
