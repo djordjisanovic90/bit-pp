46aa4f29c8b3a664dcc61d7b3e05e7543e0950da
BIT Student <classroom@bgit.rs>
2018-02-08 18:04:44 +0100

    +

--- sreda/functions.js
+++ sreda/functions.js
@@ -22,3 +22,72 @@ function isString(a) {
 }
 
 console.log(isString("hello"));
+
+//------------------------------------------
+function isblank(a)
+{
+    if (a="Number")
+    {
+        return false;
+    }
+    else if (a=="string")
+    {
+        return false;
+    }
+    else(a=="")
+    {
+        return true;
+    } 
+}
+console.log(isblank(""));
+????? ivica....................
+
+
+//stevan
+
+
+function Bstring(a){
+    /*
+    if(a == " "){
+        return true;
+    }else if(typeof a == "string"){
+        return false;
+    }else if(typeof a == "number"){
+        return false;
+    }else (typeof a == "boolean");
+        return false;
+
+    */
+
+    if ( a ===  " " ){
+        return true;
+    }
+    else{
+        return false;
+    }
+} 
+
+console.log(Bstring(false));
+//.................
+function calculator(n)
+{
+    var d = "";
+    var i;
+    var s = "ha";
+   for(i=1;i<=n;i++)
+   {
+       d=d+s;
+
+   }
+   return d;
+}
+
+console.log(calculator(3));
+//..................
+
+function brojac(c)
+{
+    var i;
+    var s = "aaaagre";
+    
+}
