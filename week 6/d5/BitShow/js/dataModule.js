de854aa2b031cced1f499d19b404975ced66c59a
BIT Student <classroom@bgit.rs>
2018-03-10 15:40:34 +0100

    subota

--- week 6/d5/BitShow/js/dataModule.js
+++ week 6/d5/BitShow/js/dataModule.js
@@ -7,6 +7,22 @@ var dataModule = (function () {
         var request = $.get(allShowsUrl);
         request.done(function (response) {
             console.log(response);
+            for (var i = 0; i < 50; i++) {
+                var show_name = response[i].name;
+                var image = response[i].image.medium;
+                var eN = image + show_name;
+                var entity = $('#content');
+
+                console.log(show_name);
+                entity.append(item);
+
+                var item = $('<div class="card" style="width: 18rem;">' +
+                    '<img class="card-img-top" src="' + image + '" alt="Card image' + 'cap">' +
+                    '<div class="card-body">' +
+                    '<p class="card-text">' + show_name + '</p>' +
+                    '</div>' +
+                    '</div>')
+            }
         });
 
 
@@ -14,18 +30,41 @@ var dataModule = (function () {
 
     });
 
-    $("#search").on('keyup',function(){
-        
-        
+    $("#search").on('keyup', function () {
+
+
+
         var searchValue = $("#search").val();
-        const showUrl = "http://api.tvmaze.com/search/shows?q=";
+        var showUrl = "http://api.tvmaze.com/search/shows?q=";
         var searchShowUrl = showUrl + searchValue;
-
         var request = $.get(searchShowUrl);
+
         request.done(function (response) {
-            console.log(response);
+            
+            
+            for (i = 0; i < response.length; i++) {
+                var entity = $('#content');
+                var entity2 = $('#content');
+                var show_name = response[i].show.name;
+                var image = response[i].show.image.medium;
+
+                var item2 = $('<div class="card" style="width: 18rem;">' +
+                    '<img class="card-img-top" src="' + image + '" alt="Card image' + 'cap">' +
+                    '<div class="card-body">' +
+                    '<p class="card-text">' + show_name + '</p>' +
+                    '</div>' +
+                    '</div>')
+
+                console.log(response);
+                entity.remove(item);
+                entity2.append(item2);
+
+            }
         });
     });
+
+    // save id of each option !!!!!!!!!!!!!!!!!
+
 
 
     //tv shows constructor
